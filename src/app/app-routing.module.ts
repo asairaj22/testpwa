d4a32db1dbabd57c46dc7ab3cb09bdf94c59448e
senthilc <schandramohan@apptium.com>
2021-05-21 13:23:24 +0530

    My Netlify Angular project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { DashboardComponent } from '../app/dashboard/dashboard.component';
+import { WelcomePageComponent } from '../app/welcome-page/welcome-page.component'
+
+const routes: Routes = [
+  { path: "dashboard", component: DashboardComponent },
+  { path: "welcome", component: WelcomePageComponent}
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
